d86e8a038adb3afa551ad85236fd1bec18b88e70
veeecu <94058192+veeecu@users.noreply.github.com>
2021-11-22 21:41:49 +0100

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,47 @@
+local ESX = nil
+
+Citizen.CreateThread(function()
+    while ESX == nil do
+        TriggerEvent('esx:getSharedObject', function(obj) 
+            ESX = obj 
+        end)
+
+        Citizen.Wait(0)
+    end
+end)
+
+RegisterCommand("tpm", function(source)
+    TeleportToWaypoint()
+end)
+
+TeleportToWaypoint = function()
+    ESX.TriggerServerCallback("veeecuTPM:fetchUserRank", function(playerRank)
+        if playerRank == "admin" or playerRank == "superadmin" or playerRank == "mod" then
+            local WaypointHandle = GetFirstBlipInfoId(8)
+
+            if DoesBlipExist(WaypointHandle) then
+                local waypointCoords = GetBlipInfoIdCoord(WaypointHandle)
+
+                for height = 1, 1000 do
+                    SetPedCoordsKeepVehicle(PlayerPedId(), waypointCoords["x"], waypointCoords["y"], height + 0.0)
+
+                    local foundGround, zPos = GetGroundZFor_3dCoord(waypointCoords["x"], waypointCoords["y"], height + 0.0)
+
+                    if foundGround then
+                        SetPedCoordsKeepVehicle(PlayerPedId(), waypointCoords["x"], waypointCoords["y"], height + 0.0)
+
+                        break
+                    end
+
+                    Citizen.Wait(5)
+                end
+
+                ESX.ShowNotification("Zostales Teleportowany.")
+            else
+                ESX.ShowNotification("Zaznacz miejsce na gps do ktorego chcesz sie tepnac.")
+            end
+        else
+            ESX.ShowNotification("Brak permisji miernoto.")
+        end
+    end)
+end
